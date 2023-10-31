1e9ed778986f9aaa2c090efbd9fe4ce9f95a0e5d
souvik9998 <souvik9998@gmail.com>
2023-10-31 15:14:13 +0530

    commit

--- authApi/server.js
+++ authApi/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: '*' }));
 
 
 
