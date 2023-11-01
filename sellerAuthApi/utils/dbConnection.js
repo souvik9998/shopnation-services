11dc1d9b6deabc97996cf20ce5cf07a1820ed9f8
souvik9998 <souvik9998@gmail.com>
2023-11-01 13:29:15 +0530

    commit

--- sellerAuthApi/utils/dbConnection.js
+++ sellerAuthApi/utils/dbConnection.js
@@ -1,3 +1,7 @@
-const mongoose =  require('mongoose');
-const connect = mongoose.connect(process.env.MONGO_URL,{useNewUrlParser :  true},mongoose.set('strictQuery',true)).then(()=>console.log("connected mongo")).catch(err=>console.log(err))
-module.exports = connect;
+// const mongoose =  require('mongoose');
+// require("dotenv").config();
+// const connect = mongoose.connect(process.env.MONGO_URL,
+//     {useNewUrlParser :  true},mongoose.set('strictQuery',true))
+//     .then(()=>console.log("connected mongo"))
+//     .catch(err=>console.log(err))
+// module.exports = connect;
