72282d4a465f5871c22a63324cdc05dbc3a4c723
souvik9998 <souvik9998@gmail.com>
2023-12-02 22:23:40 +0530

    commit

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -20,43 +20,43 @@ const Services = () => {
             <div className=' w-full h-full grid grid-rows-2 lg:grid-rows-3 grid-cols-5 lg:grid-cols-3 lg:gap-2 lg:px-2'>
               <div className='flex flex-col items-center justify-around p-4 hover:shadow-md hover:border-[1px] hover:border-slate-200/90 hover:bg-slate-200/80 rounded-xl'>
                 <img className='h-fit w-fit' src={maid}></img>
-                <div className='text-sm font-semibold text-center'>Maid service</div>
+                <div className='text-xs lg:text-sm font-semibold text-center'>Maid service</div>
               </div>
               <div className='flex flex-col items-center justify-around p-4 hover:shadow-md hover:border-[1px] hover:border-slate-200/90 hover:bg-slate-200/80 rounded-xl'>
                 <img className='h-fit w-fit' src={chef}></img>
-                <div className='text-sm font-semibold text-center'>Cook</div>
+                <div className='text-xs lg:text-sm font-semibold text-center'>Cook</div>
               </div>
               <div className='flex flex-col items-center justify-around p-4 hover:shadow-md hover:border-[1px] hover:border-slate-200/90 hover:bg-slate-200/80 rounded-xl'>
                 <img className='h-fit w-fit' src={plumber}></img>
-                <div className='text-sm font-semibold text-center'>Plumber</div>
+                <div className='text-xs lg:text-sm font-semibold text-center'>Plumber</div>
               </div>
               <div className='flex flex-col items-center justify-around p-4 hover:shadow-md hover:border-[1px] hover:border-slate-200/90 hover:bg-slate-200/80 rounded-xl'>
                 <img className='h-fit w-fit' src={carpenter}></img>
-                <div className='text-sm font-semibold text-center'>Carpenter</div>
+                <div className='text-xs lg:text-sm font-semibold text-center'>Carpenter</div>
               </div>
               <div className='flex flex-col items-center justify-around p-4 hover:shadow-md hover:border-[1px] hover:border-slate-200/90 hover:bg-slate-200/80 rounded-xl'>
                 <img className='h-fit w-fit' src={teacher}></img>
-                <div className='text-sm font-semibold text-center'>Teaching</div>
+                <div className='text-xs lg:text-sm font-semibold text-center'>Teaching</div>
               </div>
               <div className='flex flex-col items-center justify-around p-4 hover:shadow-md hover:border-[1px] hover:border-slate-200/90 hover:bg-slate-200/80 rounded-xl'>
                 <img className='h-fit w-fit' src={firefigher}></img>
-                <div className='text-sm font-semibold text-center'>Fire fighter</div>
+                <div className='text-xs lg:text-sm font-semibold text-center'>Fire fighter</div>
               </div>
               <div className='flex flex-col items-center justify-around p-4 hover:shadow-md hover:border-[1px] hover:border-slate-200/90 hover:bg-slate-200/80 rounded-xl'>
                 <img className='h-fit w-fit' src={electrician}></img>
-                <div className='text-sm font-semibold text-center'>Electrician</div>
+                <div className='text-xs lg:text-sm font-semibold text-center'>Electrician</div>
               </div>
               <div className='flex flex-col items-center justify-around p-4 hover:shadow-md hover:border-[1px] hover:border-slate-200/90 hover:bg-slate-200/80 rounded-xl'>
                 <img className='h-fit w-fit' src={barber}></img>
-                <div className='text-sm font-semibold text-center'>Barber</div>
+                <div className='text-xs lg:text-sm font-semibold text-center'>Barber</div>
               </div>
               <div className='flex flex-col items-center justify-around p-4 hover:shadow-md hover:border-[1px] hover:border-slate-200/90 hover:bg-slate-200/80 rounded-xl'>
                 <img className='h-fit w-fit' src={builder}></img>
-                <div className='text-sm font-semibold text-center'>Builder</div>
+                <div className='text-xs lg:text-sm font-semibold text-center'>Builder</div>
               </div>
               <div className='lg:hidden flex flex-col items-center justify-around p-4 hover:shadow-md hover:border-[1px] hover:border-slate-200/90 hover:bg-slate-200/80 rounded-xl'>
                 <img className='h-fit w-fit' src={programmer}></img>
-                <div className='text-sm font-semibold text-center'>Programmer</div>
+                <div className='text-xs lg:text-sm px-2 font-semibold text-center'>Programmer</div>
               </div>
             </div>
             <div className='flex justify-center items-center'>
