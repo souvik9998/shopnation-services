801106aa436a2558cbc21ba91ecee118efb9d1bb
souvik9998 <souvik9998@gmail.com>
2023-12-27 20:57:07 +0530

    commit

--- client/src/pages/User/Profile/ProfileLayout.js
+++ client/src/pages/User/Profile/ProfileLayout.js
@@ -5,9 +5,9 @@ import { useGlobalContext } from '../../../components/context'
 const ProfileLayout = () => {
   return (
     <>
-      <div className='lg:min-w-screen min-h-screen bg-gray-200/50 lg:bg-cardColor flex gap-10 justify-center items-start lg:p-10 overflow-auto'>
+      <div className='lg:min-w-screen h-fit lg:min-h-screen bg-gray-200/50 lg:bg-cardColor flex gap-10 justify-center items-start lg:p-10 overflow-auto'>
         <div className='lg:block hidden w-[20%] bg-white profileShadow'><ProfileNavigation/></div>
-        <div className='w-full lg:w-7/12 h-full lg:bg-white profileShadow'><Outlet /></div>
+        <div className='w-full lg:w-7/12 h-fit lg:bg-white profileShadow overflow-auto'><Outlet /></div>
       </div>
     </>
   )
