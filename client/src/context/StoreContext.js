b2d708ff74d66ab89e1e8278f4d9bb9014f5ae5d
souvik9998 <souvik9998@gmail.com>
2024-01-13 00:42:12 +0530

    commit

--- client/src/context/StoreContext.js
+++ client/src/context/StoreContext.js
@@ -7,8 +7,12 @@ const StoreProvider = ({children})=>{
     const [shopInfo,setShopInfo] = useState('');
     const [productList,setProductList] = useState([]);
     const [currentShopId,setCurrentShopId] = useState('');
+    const [currentProduct,setCurrentProduct] = useState('');
+    const [defaultProduct,setDefaultProduct] = useState('');
+    const [variants,setVariants] = useState([]);
+    const [variantDetail,setVariantDetail] = useState([])
     return <StoreContext.Provider
-    value={{shopInfo,setShopInfo,productList,setProductList,currentShopId,setCurrentShopId}}>
+    value={{variantDetail,setVariantDetail,variants,setVariants,currentProduct,setCurrentProduct,defaultProduct,setDefaultProduct,shopInfo,setShopInfo,productList,setProductList,currentShopId,setCurrentShopId}}>
         {children}
     </StoreContext.Provider>
 }
--- client/src/pages/ProductInfopage.js
+++ client/src/pages/ProductInfopage.js
@@ -12,57 +12,66 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "swiper/css/grid";
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import { StoreProvider, useStoreContext } from '../context/StoreContext';
 import '../App.css';
 
 import { Pagination} from "swiper";
 const ProductInfopage = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const {shopId,productId} = useParams();
+  const {shopId,productId,variantId} = useParams();
   const [variantName,setVariantName] = useState('');
-  const [variantId,setVariantId] = useState('');
+  // const [variantId,setVariantId] = useState('');
   const [sizeName,setSizeName] = useState('');
-  const [defualtProductId,setDefaultProductId] = useState('');
-  const [item,setItem] = useState();
-  const [variants,setVariants] = useState([])
+  // const [item,setItem] = useState();
   const [sizeArray,setSizeArray] = useState([]);
-  const [defaultItem,setDefaultItem] = useState();
   const [mainImage,setMainImage] = useState('');
   const [productImages,setProductImages] = useState([]);
   const token = window.localStorage.getItem("token");
   const {calculateExpectedDelivery} = useGlobalContext();
   const {cartList,user,setCartList,getCartDetails,getCartProductInfo} = useGlobalContext();
+  const {variants,setVariants,defaultProduct,setDefaultProduct,currentProduct,setCurrentProduct} = useStoreContext();
   useEffect(()=>{
-      const variant_name = window.localStorage.getItem("variant_name");
-      const main_product = JSON.parse(window.localStorage.getItem("main_product"));
-      if(variant_name){
-        console.log(variant_name);
-        setVariantName(variant_name);
-        setDefaultItem(main_product.item);
-        setAdditional(main_product);
-        showProductInfo(variant_name);
+      // const variant_name = window.localStorage.getItem("variant_name");
+      
+      // if(variant_name){
+      //   console.log(variant_name);
+      //   setVariantName(variant_name);
+      //   setDefaultItem(main_product.item);
+      //   setAdditional(main_product);
+      //   showProductInfo(variant_name);
+      // }
+      // else showProductInfo();
+      if(!defaultProduct && location.pathname === `/ShopProductPage/${shopId}/${productId}/${variantId}`){
+        const default_product = JSON.parse(window.localStorage.getItem("default_product"));
+        setAdditional(default_product);
       }
-      else showProductInfo();
+      showProductInfo();
   },[location.pathname,variantId,productId])
 
-  const showProductInfo = async(variantName)=>{
+  const showProductInfo = async()=>{
     if(location.pathname === `/ShopProductPage/${shopId}/${productId}`){
-      
-      try{  
-        const res =  await axios.get(`https://${baseUrl}/sellerapi/onboard/showProduct/${productId}`);
-        console.log(res);
-        setItem(res.data.item);
-        // setDefaultProductId(productId);
-        setAdditional(res.data);
+      if(!defaultProduct || defaultProduct.item.productId != productId){
+        try{  
+          const res =  await axios.get(`https://${baseUrl}/sellerapi/onboard/showProduct/${productId}`);
+          console.log(res);
+          setCurrentProduct(res.data.item);
+          // setDefaultProductId(productId);
+          setAdditional(res.data);
+        }
+        catch(err){
+          console.log(err);
+        }
       }
-      catch(err){
-        console.log(err);
+      else{
+        setCurrentProduct(defaultProduct.item);
+        setAdditional(defaultProduct);
       }
     }
     else{
         try{
-            const res = await axios.get(`https://${baseUrl}/sellerapi/onboard/getProductVariantInfo/${productId}?variantName=${variantName}`);
+            
+            const res = await axios.get(`https://${baseUrl}/sellerapi/onboard/getVariantInfo/${variantId}`);
             const newSizeArray = [];
             variants.forEach((v)=>{
             v.typeArray.forEach((i)=>{
@@ -78,11 +87,10 @@ const ProductInfopage = () => {
               setSizeArray([res.data.item.size,...newSizeArray]);
             }
           
-          setItem(res.data.item);
+          setCurrentProduct(res.data.item);
           setSizeName(res.data.item.size);
           setMainImage(res.data.item.mainImagePath);
           setProductImages(res.data.item.productImages);
-          localStorage.setItem("variant_name",variantName);
           
         }
         catch(err){
@@ -90,33 +98,13 @@ const ProductInfopage = () => {
         }
     }
   }
-  const handleVariantClick = (variant) =>{
-    if(variant.variantName === defaultItem.variantName){
-      setVariantId(defaultItem.productId);
-      setVariantName(defaultItem.variantName);
-      navigate(`/ShopProductPage/${shopId}/${productId}`);
-    }
-    else{
-      let id;
-      variant.variantArray.forEach((v)=>{
-        if(v.size === item.size){ 
-          id = v.variantId;
-        }
-      })
-      setVariantId(id);
-      setVariantName(variant.variantName);
-      navigate(`/ShopProductPage/${shopId}/${productId}/${id}`)
-    }
-    
-  }
   const setAdditional =(data)=>{
-      setDefaultItem(data.item);
+      setDefaultProduct(data);
       setVariantName(data.item.variantName);
       setMainImage(data.item.mainImagePath);
       setProductImages(data.item.productImages);
       setSizeName(data.item.size);
-      window.localStorage.setItem("main_product",JSON.stringify(data));
-      // setVariants(data.variantObject);
+      window.localStorage.setItem("default_product",JSON.stringify(data));
       const newSizeArray = [];
       const variantArray = [];
       Object.entries(data.variantObject).forEach(([key,value])=>{
@@ -130,7 +118,6 @@ const ProductInfopage = () => {
             value.forEach((i)=>{
               if(i.variantName === data.item.variantName){
                 i.variantArray.forEach((j)=>{
-                  console.log(j);
                   newSizeArray.push(j.size);
                 })
               }
@@ -147,9 +134,29 @@ const ProductInfopage = () => {
         else setSizeArray([data.item.size,...newSizeArray]);
       }
       
-      setVariants([...variantArray]);
+      setVariants([...variantArray])
   }
  
+  const handleVariantClick = (variant) =>{
+    if(variant.variantName === defaultProduct.item.variantName){
+      // setVariantId(defaultProduct.productId);
+      setVariantName(defaultProduct.variantName);
+      navigate(`/ShopProductPage/${shopId}/${productId}`);
+    }
+    else{
+      let id;
+      variant.variantArray.forEach((v)=>{
+        if(v.size === currentProduct.size){ 
+          id = v.variantId;
+        }
+      })
+      // setVariantId(id);
+      setVariantName(variant.variantName);
+      navigate(`/ShopProductPage/${shopId}/${productId}/${id}`)
+    }
+    
+  }
+
 
 
   const handleImageHover =(imagePath)=>{
@@ -159,13 +166,13 @@ const ProductInfopage = () => {
     try{
       const res1 =  await axios
       .post(`https://${baseUrl}/userapi/cart/addToCart`,{
-        productId : item.productId,
+        productId : currentProduct.productId,
         userId: user.userId,
-        productName : item.productName,
-        productAmount : item.productPrice,
-        productType : item.productType,
+        productName : currentProduct.productName,
+        productAmount : currentProduct.productPrice,
+        productType : currentProduct.productType,
         shopId : shopId,
-        expectedDelivery:calculateExpectedDelivery(item.expectedDelivery),
+        expectedDelivery:calculateExpectedDelivery(currentProduct.expectedDelivery),
         quantity : 1
       },
       {
@@ -181,21 +188,20 @@ const ProductInfopage = () => {
       console.log(err);
     }
     }
-    
-
+    console.log(variants);
   return (
     <>
-    {item? 
+    {currentProduct? 
       <div className='w-screen min-h-screen font-Inter '>
         <div className='hidden lg:flex gap-10 h-fit w-full px-16 pt-16 py-10'>
           <div className='w-[45%] h-96 px-2 flex justify-center gap-2'>
           <div className='w-[25%] h-full px-4 flex flex-col gap-2 overflow-scroll'>
             {
-                <div className={`w-full h-[25%] rounded-sm ${mainImage === item.mainImagePath? 'border-2 border-blue-500 ':''}`}>
+                <div className={`w-full h-[25%] rounded-sm ${mainImage === currentProduct.mainImagePath? 'border-2 border-blue-500 ':''}`}>
                 <img
                 className='rounded-sm w-full h-full object-contain  overflow-hidden'
-                onMouseEnter={()=>handleImageHover(item.mainImagePath)}  
-                src={item.mainImagePath.url}></img></div>
+                onMouseEnter={()=>handleImageHover(currentProduct.mainImagePath)}  
+                src={currentProduct.mainImagePath.url}></img></div>
             }
             {
               (productImages)?
@@ -214,9 +220,9 @@ const ProductInfopage = () => {
           </div>
           <div className='w-fit max-w-96 flex flex-col h-fit gap-3'>
             <div className='bg-gray-100 text-green-600 font-semibold text-sm w-fit rounded-sm px-3 py-[0.8px]'>{
-                (item.isAvailable)?'In Stock':'Out of stock'
+                (currentProduct.isAvailable)?'In Stock':'Out of stock'
             }</div>
-            <div className='capitalize font-semibold text-2xl'>{item.productName} {item.productDescription}</div>
+            <div className='capitalize font-semibold text-2xl'>{currentProduct.productName} {currentProduct.productDescription}</div>
             <div className='flex gap-2 items-center '>
             <div className='flex gap-[2px] bg-green-600 text-white rounded-sm w-12 text-xs items-center justify-center'>
               <div className='py-1'>4.2</div>
@@ -225,7 +231,7 @@ const ProductInfopage = () => {
             <div className='text-gray-500 font-semibold text-sm'>(1000 reviews)</div>
             </div>
             <div className='font-semibold text-3xl items-end flex gap-3 h-12'>
-              <div>₹{item.productPrice}</div>
+              <div>₹{currentProduct.productPrice}</div>
               <div className='relative'><div className='text-base text-gray-400'>₹3000</div>
               <div className='absolute inset-y-1/2 w-full h-[2px] rounded-lg bg-gray-600'></div>
               </div>
@@ -255,15 +261,14 @@ const ProductInfopage = () => {
             {
               variants.map((variant)=>{
                 return <div className='flex flex-col gap-4 w-[85%] h-36 justify-center'>
-                <div className='text-sm font-semibold text-gray-700 uppercase'>{variant.variantType}:<span className='pl-2 text-gray-600 font-medium'>{item.variantName}</span></div>
+                <div className='text-sm font-semibold text-gray-700 uppercase'>{variant.variantType}:<span className='pl-2 text-gray-600 font-medium'>{currentProduct.variantName}</span></div>
                 <div className='flex items-center gap-2'>
                 {
                 variant.typeArray.map((v) =>{
-                  console.log(v);
                   if(variant.variantType === "color"){
                     return <div 
                     onClick={()=>handleVariantClick(v)}
-                    className={`${v.variantName === item.variantName ? 'border-gray-700' : 'border-gray-300'} border-2 rounded-md cursor-pointer`}>
+                    className={`${v.variantName === currentProduct.variantName ? 'border-gray-700' : 'border-gray-300'} border-2 rounded-md cursor-pointer`}>
                       <img className='h-20 w-24 object-contain' src={v.variantArray[0].variantMainImage.url}></img>
                     </div>
                   }
@@ -278,11 +283,11 @@ const ProductInfopage = () => {
           }
             <div className='flex gap-3 items-center'>
             <div className='text-slate-700'><ion-icon name="bicycle" style={{fontSize:'22px'}}></ion-icon></div>
-            <div className='font-semibold text-slate-600 text-sm'>Expected delivery in <span className='text-black'>{calculateExpectedDelivery(item.expectedDelivery)}. </span><span className='underline'>Want it faster?</span></div>
+            <div className='font-semibold text-slate-600 text-sm'>Expected delivery in <span className='text-black'>{calculateExpectedDelivery(currentProduct.expectedDelivery)}. </span><span className='underline'>Want it faster?</span></div>
             </div>
             <div className='flex gap-3 items-center'>
             <div className='text-slate-700'><ion-icon name="home" style={{fontSize:'22px'}}></ion-icon></div>
-            <div className='font-semibold text-slate-600 text-sm'>Sold by <span className='text-black'>{item.shopName}. </span><span className='underline'>Contact shop.</span></div></div>
+            <div className='font-semibold text-slate-600 text-sm'>Sold by <span className='text-black'>{currentProduct.shopName}. </span><span className='underline'>Contact shop.</span></div></div>
           
             <div className='flex gap-3 items-center'>
             <div className='text-slate-700'><ion-icon name="wallet" style={{fontSize:'22px'}}></ion-icon></div>
@@ -320,8 +325,8 @@ const ProductInfopage = () => {
 
           </div>
           <div>
-          <div className='capitalize text-gray-800 font-bold text-lg'>{item.productName}</div>
-          <div className='capitalize text-gray-500 font-semibold text'>{item.productDescription}</div>
+          <div className='capitalize text-gray-800 font-bold text-lg'>{currentProduct.productName}</div>
+          <div className='capitalize text-gray-500 font-semibold text'>{currentProduct.productDescription}</div>
           </div>
         
           <div className='w-full h-[52vh]'>
@@ -330,7 +335,7 @@ const ProductInfopage = () => {
                 <SwiperSlide className={`w-full h-full rounded-sm`}>
                 <img
                 className='rounded-sm w-full h-[85%] object-contain  overflow-hidden'
-                src={item.mainImagePath.url}></img></SwiperSlide>
+                src={currentProduct.mainImagePath.url}></img></SwiperSlide>
             }
             {
               (productImages)?
@@ -352,11 +357,10 @@ const ProductInfopage = () => {
                 <div className='flex items-center gap-2'>
                 {
                 variant.typeArray.map((v) =>{
-                  console.log(v);
                   if(variant.variantType === "color"){
                     return <div 
                     onClick={()=>handleVariantClick(v)}
-                    className={`${v.variantName === item.variantName ? 'border-gray-700' : 'border-gray-300'} border-2 rounded-md cursor-pointer`}>
+                    className={`${v.variantName === currentProduct.variantName ? 'border-gray-700' : 'border-gray-300'} border-2 rounded-md cursor-pointer`}>
                       <img className='h-20 w-24 object-contain' src={v.variantArray[0].variantMainImage.url}></img>
                     </div>
                   }
@@ -386,11 +390,11 @@ const ProductInfopage = () => {
             <div className='flex flex-col gap-2 py-4'>
             <div className='flex gap-3 items-center'>
             <div className='text-slate-700'><ion-icon name="bicycle" style={{fontSize:'22px'}}></ion-icon></div>
-            <div className='font-semibold text-slate-600 text-sm'>Expected delivery in <span className='text-black'>{calculateExpectedDelivery(item.expectedDelivery)}. </span><span className='underline'>Want it faster?</span></div>
+            <div className='font-semibold text-slate-600 text-sm'>Expected delivery in <span className='text-black'>{calculateExpectedDelivery(currentProduct.expectedDelivery)}. </span><span className='underline'>Want it faster?</span></div>
             </div>
             <div className='flex gap-3 items-center'>
             <div className='text-slate-700'><ion-icon name="home" style={{fontSize:'22px'}}></ion-icon></div>
-            <div className='font-semibold text-slate-600 text-sm'>Sold by <span className='text-black'>{item.shopName}. </span><span className='underline'>Contact shop.</span></div></div>
+            <div className='font-semibold text-slate-600 text-sm'>Sold by <span className='text-black'>{currentProduct.shopName}. </span><span className='underline'>Contact shop.</span></div></div>
           
             <div className='flex gap-3 items-center'>
             <div className='text-slate-700'><ion-icon name="wallet" style={{fontSize:'22px'}}></ion-icon></div>
